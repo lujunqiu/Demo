58950881b0357e912ae393fb5b33a5e9c5f1efeb
komine <2672543426@qq.com>
2016-09-22 18:53:29 +0800

    1

--- src/MaxSumOfSubArray.java
+++ src/MaxSumOfSubArray.java
@@ -1,6 +1,7 @@
 /**
- * Created by Administrator on 2016/9/22.
+ * Created by Lujunqiu on 2016/9/22.
+ *一个有N个整数元素的一位数组(A[0], A[1],...,A[n-1], A[n])，这个数组当然有很多子数组，要求找出数组连续下标和的最大值
  */
 public class MaxSumOfSubArray {
-    
+
 }
