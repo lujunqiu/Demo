f673479168c92e4b7e0eb2bbf5c65d20970cf267
komine <2672543426@qq.com>
2016-11-01 10:25:00 +0800

    11

---
+++ src/datastructure/ChangTwoInteger.java
@@ -0,0 +1,17 @@
+package datastructure;
+
+/**
+ * Created by Lujunqiu on 2016/11/1.
+ * 给定两个(基本数据类型的包装类)Integer类型的变量a和b，要求实现一个函数swap，交换他们的值。
+ */
+public class ChangTwoInteger {
+    public static void  main(String[] args) {
+        Integer a = 1, b = 2 ;
+        System.out.println("before swap a = " + a + "b = " + b);
+        swap(a , b);
+        System.out.println("after swap a = " + a + "b = " + b);
+    }
+    public static void swap(Integer i , Integer j){
+
+    }
+}
